29e90257db3ddff08754a16c1aec7db276a1dca1
kis6905 <kis6905@gmail.com>
2020-07-10 00:05:19 +0900

    주석 제거

--- src/components/ListCardType.vue
+++ src/components/ListCardType.vue
@@ -28,38 +28,6 @@
         </div>
       </div>
     </v-card>
-    <!--
-    <b-card
-      v-for="(item, index) in items"
-      :key="index"
-      class="card"
-      bg-variant="dark"
-      text-variant="white"
-      border-variant="secondary"
-      @click="$emit('click-item', item)">
-      <div class="card-item">
-        <div class="title">
-          {{ item.title }}
-        </div>
-        <div class="arrow-right">
-          <b-icon-chevron-right></b-icon-chevron-right>
-        </div>
-      </div>
-    </b-card>
-    <b-card
-      v-if="!items || items.length === 0"
-      class="empty-card"
-      bg-variant="dark"
-      text-variant="white"
-      border-variant="secondary">
-      <div class="empty-card-body text-center">
-        <div class="empty-card-body-content">
-          <b-icon-exclamation-circle :font-scale="1"></b-icon-exclamation-circle>
-          데이터가 없습니다!
-        </div>
-      </div>
-    </b-card>
-    -->
   </div>
 </template>
 
